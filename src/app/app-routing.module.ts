5f9a21e527ae15c955bb249118899593d5f3c89a
noble-wave <dev@noble-wave.com>
2023-07-22 19:30:51 +0530

    routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'pro', loadChildren: () => import('./protected/protected.module').then(m => m.ProtectedModule) },
+  { path: 'home', loadChildren: () => import('./open/open.module').then(m => m.OpenModule) },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
