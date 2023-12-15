11e5a98955a52e47915409f5a5ea0f766f9ed220
nitishriddler <nitishkumar987654@gmail.com>
2023-12-15 22:24:22 +0530

    minor change

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ProductComponent } from './protected/product/product.component';
 
 const routes: Routes = [
   // { path: '', loadChildren: () => import('./public/public.module').then(m => m.PublicModule) },
