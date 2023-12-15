643953e7faa1583914823c977a0eb285bb2b6ef3
nitishriddler <nitishkumar987654@gmail.com>
2023-12-15 22:22:38 +0530

    path set to product

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductComponent } from './protected/product/product.component';
 
 const routes: Routes = [
-  { path: '', loadChildren: () => import('./public/public.module').then(m => m.PublicModule) },
+  // { path: '', loadChildren: () => import('./public/public.module').then(m => m.PublicModule) },
+  { path: '', redirectTo: '/product', pathMatch: 'full' },
   { path: '', loadChildren: () => import('./protected/protected.module').then(m => m.ProtectedModule) },
-  { path: '**', redirectTo: '/login' }
+  // { path: '**', redirectTo: '/login' }
+  { path: '**', redirectTo: '/product' }
 ];
 
 @NgModule({
