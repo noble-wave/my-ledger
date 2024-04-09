ead6cc404b6f5918f0601a0b099fb52b7890513a
Nitish-Repo <nitishkumar987654@gmail.com>
2024-04-09 18:08:45 +0530

    landing page change

--- src/app/protected/dashboard/dashboard.component.ts
+++ src/app/protected/dashboard/dashboard.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { Location } from '@angular/common';
-
+import { SellService } from '../services/sell.service';
+import { Router } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogComponent } from '@app/shared/controls/template/dialog/dialog.component';
 
 @Component({
   selector: 'app-dashboard',
@@ -8,13 +11,57 @@ import { Location } from '@angular/common';
   styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent {
+  dataPresent: boolean = false;
 
-  constructor( private location: Location,) {}
+  constructor(
+    private location: Location,
+    private sellService: SellService,
+    public dialog: MatDialog,
+    private router: Router
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.sellService.isDataPresent().subscribe((dataPresent) => {
+      this.dataPresent = dataPresent;
+      const isFirstVisit = localStorage.getItem('isFirstVisitProductList');
+      if (!isFirstVisit || !dataPresent) {
+        localStorage.setItem('isFirstVisitProductList', 'true');
+        this.demoData('enterAnimationDuration', 'exitAnimationDuration');
+      }
+    });
+  }
 
-  navigateBack(){
+  navigateBack() {
     this.location.back();
   }
 
+  async demoData(
+    enterAnimationDuration: string,
+    exitAnimationDuration: string
+  ) {
+    const dialogConfig = {
+      width: '600px',
+      enterAnimationDuration,
+      exitAnimationDuration,
+      data: {
+        dialogTitle: 'Welcome to Ledger App',
+        dialogContent: `
+      This application simplifies invoice generation, enables insightful sales analysis,
+      and facilitates efficient product and customer management.<br><br>
+      Would you like to explore the app with pre-populated data for a comprehensive understanding, or start with a clean slate?
+    `,
+        cancelButtonText: 'Start Fresh',
+        confirmButtonText: 'Explore with Sample Data',
+        color: 'primary',
+      },
+    };
+
+    const dialogRef = this.dialog.open(DialogComponent, dialogConfig);
+
+    dialogRef.afterClosed().subscribe(async (result) => {
+      if (result === 'true') {
+        this.router.navigate(['/dummy-data-loader']);
+      }
+    });
+  }
 }
