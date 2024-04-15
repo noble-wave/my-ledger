c86c5c8768743b3ba9c732a2d432ba86239ccca9
Nitish-Repo <nitishkumar987654@gmail.com>
2024-04-15 15:46:31 +0530

    fix responsive dashboard and setting page and import/export

--- src/app/shared/controls/reactive/typeahead/typeahead.component.ts
+++ src/app/shared/controls/reactive/typeahead/typeahead.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit, Input, Output, EventEmitter, ViewChild } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  Output,
+  EventEmitter,
+  ViewChild,
+} from '@angular/core';
 import { FormControl, AbstractControl } from '@angular/forms';
 import { MatAutocompleteTrigger } from '@angular/material/autocomplete';
 import { FloatLabelType } from '@angular/material/form-field';
@@ -30,8 +37,9 @@ export class IrsTypeaheadComponent implements OnInit {
   @Output() public debounceKeyup = new EventEmitter<string>();
   @Output() onSelectionChange = new EventEmitter<any>(true);
   @Output() navigateClick: EventEmitter<void> = new EventEmitter<void>();
-  @ViewChild(MatAutocompleteTrigger, { static: true }) trigger: MatAutocompleteTrigger;
-  
+  @ViewChild(MatAutocompleteTrigger, { static: true })
+  trigger: MatAutocompleteTrigger;
+
   filteredOptions: any[];
 
   constructor() {}
@@ -70,6 +78,7 @@ export class IrsTypeaheadComponent implements OnInit {
 
   onKeyup(event?: any) {
     let searchTxt = event?.target?.value;
+    console.log('user Input:', searchTxt);
     this.filteredOptions = this._filter(searchTxt);
     this.control.setValue(null);
     this.searchUpdated.next(searchTxt);
@@ -94,6 +103,8 @@ export class IrsTypeaheadComponent implements OnInit {
   selectionChange(option) {
     // console.log(event);
     // let option = this.options.find(x => x[this.optValueLabel] == event.value);
+    console.log('selected option:', option);
+    console.log('updated textValue:', option[this.optTextLabel]);
     this.viewValue = option[this.optTextLabel];
     this.control.setValue(option[this.optValueLabel]);
     this.onSelectionChange.emit(option);
